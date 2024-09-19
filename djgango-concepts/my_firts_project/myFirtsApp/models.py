46b241b3ccf36303c49aaeed4b05dc804d48eac7
Jorge Mario Sanchez <costos@comersantander.local>
2024-09-19 17:39:36 -0500

    primera muestra de pagina de elementos de la vista con for

--- djgango-concepts/my_firts_project/myFirtsApp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-# The first model is a car
-class Car(models.Model):
-    title = models.TextField(max_length=250) # max of data receive
--- djgango-concepts/my_firts_project/myFirtsApp/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def my_view(request):
-    render(request,'car_list.html')
